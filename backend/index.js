1107e7314926c994025d985ff5176969862b9c99
Sanjay P <sanjayprasanna56@gmail.com>
2025-10-09 23:42:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:["https://onecart-1-slb0.onrender.com","http://localhost:5174"],
+  origin:["https://onecart-1-slb0.onrender.com","https://onecart-admin-slnf.onrender.com"],
   credentials:true
 }))
 
