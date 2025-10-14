e48818abe2a77861fa0b0f5e926e23e499751542
Sanjay P <sanjayprasanna56@gmail.com>
2025-10-14 18:21:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,50 +3,32 @@ import dotenv from 'dotenv';
 import connectDb from './config/db.js';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoute.js';
-import cors from 'cors';
+import cors from 'cors'
 import userRoutes from './routes/userRoute.js';
 import productRoutes from './routes/productRoute.js';
 import cartRoutes from './routes/cartRoute.js';
 import orderRoutes from './routes/orderRoute.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-dotenv.config();
+dotenv.config()
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+let port=process.env.PORT;
 
-const app = express();
-let port = process.env.PORT || 5000;
+const app=express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: [
-    "https://onecart-1-slb0.onrender.com",
-    "https://onecart-admin-slnf.onrender.com"
-  ],
-  credentials: true
-}));
-app.options("*", cors());
+  origin:["http://localhost:5173","http://localhost:5174"],
+  credentials:true
+}))
 
-// API routes
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/product", productRoutes);
-app.use("/api/cart", cartRoutes);
-app.use("/api/order", orderRoutes);
+app.use("/api/auth",authRoutes);
+app.use("/api/user",userRoutes);
+app.use("/api/product",productRoutes);
+app.use("/api/cart",cartRoutes);
+app.use("/api/order",orderRoutes);
 
-// Serve frontend build (React/Vite SPA)
-app.use(express.static(path.join(__dirname, "frontend/build"))); // adjust path if needed
-
-// Catch-all route to serve index.html for SPA routing
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
-});
-
-// Start server & connect DB
-app.listen(port, () => {
+app.listen(port,()=>{
   console.log(`Server is running on port ${port}`);
   connectDb();
-});
+})
