736497c3cb6b8344be4ce0d11d393a21171866af
Sanjay P <sanjayprasanna56@gmail.com>
2025-10-16 17:57:55 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -70,13 +70,18 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("token");
-    return res.status(200).json({ message: "Logout successfull" });
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
+    return res.status(200).json({ message: "Logout successful" });
   } catch (error) {
     console.log("logout error");
     return res.status(500).json({ message: `logout error ${error}` });
   }
 };
+
 
 export const googleLogin = async (req, res) => {
   try {
