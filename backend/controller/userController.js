c1eec172257fa23555f87dea7a67f74357fb042c
Sanjay P <sanjayprasanna56@gmail.com>
2025-10-16 18:03:09 +0530

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,18 +1,30 @@
 import User from '../model/userModel.js'
 
-export const getCurrentUser=async(req,res)=>{
-  try {
-    let user=await User.findById(req.userId).select("-password")
-    if(!user){
-       return res.status(404).json({message:"user is not found"})
-    }
+// export const getCurrentUser=async(req,res)=>{
+//   try {
+//     let user=await User.findById(req.userId).select("-password")
+//     if(!user){
+//        return res.status(404).json({message:"user is not found"})
+//     }
 
-     return res.status(200).json(user)
+//      return res.status(200).json(user)
+//   } catch (error) {
+//      console.log("error");
+//     return res.status(500).json({ message: `getCurrentUser error ${error}` });
+//   }
+// }
+const getCurrentUser = async () => {
+  try {
+    const response = await axios.get(`${serverUrl}/api/auth/getCurrentUser`, {
+      withCredentials: true,
+    });
+    setUserData(response.data);
   } catch (error) {
-     console.log("error");
-    return res.status(500).json({ message: `getCurrentUser error ${error}` });
+    console.log("No active session");
+    setUserData(null); // 🔥 ensure context resets when cookie is gone
   }
-}
+};
+
 
 export const getAdmin=async(req,res)=>{
   try {
